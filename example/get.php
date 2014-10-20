ceb1bfd8a44313b62f3f25ec8fd049d887e85d81
David Hernandez <rapmasteer@gmail.com>
2014-10-20 15:21:30 -0500

    0.2
    ocultando llaves

--- example/get.php
+++ example/get.php
@@ -1,19 +1,10 @@
 <?php
 require "twitteroauth/twitteroauth.php";
-$consumerkey = "zM28XVkmrtPQQi1HhSSKQ";
-$consumersecret = "yIyqbpPHybzVVzt4SQ4IMxR3WwYl9o2RK6ASP19AE";
-$accesstoken = "128752572-RwxH1lXl5t8xp9ox1yrR58j93zFiOGXP3rtJso";
-$accesstokensecret = "ec0zhXrGatNXEJJjFOaPVmojjdjwOa7vIoMA0pu0ug";
+$consumerkey = "XXXXXXXXXXXXXXXXXXX";
+$consumersecret = "XXXXXXXXXXXXXXXXXXXXXXXX";
+$accesstoken = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$accesstokensecret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
-function tweet_text($text) {
-	// urls: lo primero para que no conviertan los links posteriores
-	$text =  preg_replace("/(http:\/\/[^\s]+)/", '<a href="$1" class="tweet-link" target="_blank">$1</a>', $text);
-	// menciones
-	$text =  preg_replace("/(^|\W)@([A-Za-z0-9_]+)/", '$1<a href="http://twitter.com/$2" class="tweet-mention" target="_blank">@$2</a>', $text);
-	// hashtags
-	$text =  preg_replace("/(^|\W)#([^\s]+)/", '$1<a href="?q=%23$2" class="tweet-hash" target="_blank">#$2</a>', $text);
-	return $text;
-}
 function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
   $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
   return $connection;
